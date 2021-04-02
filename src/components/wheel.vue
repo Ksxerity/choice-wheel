83eca5ee8fb6aac2d6ae5dd150fe7d18bcc0d3de
Kyle Ki <kki@pop-os.localdomain>
2021-04-01 20:32:39 -0400

    Added alternating colors for the wedges. Added a min and max value for the input. Slightly modified the perimeter/offset to make the wedges fit better. Also added a label placeholder.

--- src/components/wheel.vue
+++ src/components/wheel.vue
@@ -5,16 +5,33 @@
         class="wedge"
         :style="`
           transform: rotate(${wedgeAngle * index}deg);
+          border-color: transparent ${colorController(
+            index
+          )} transparent transparent;
           --radius: ${radius}px;
           --wedge-perimeter: ${wedgePerimeter}px;
+          --wedge-offset: -${wedgePerimeter - 1}px;
         `"
         v-for="index in slots"
         :key="index"
-      ></div>
+      >
+        <label
+          class="wedge-label"
+          :style="`--label-height: ${wedgePerimeter * 2}px`"
+        >
+          asd
+        </label>
+      </div>
     </div>
     <div class="settings">
       <label>How many entries?</label>
-      <input class="slots-entry" type="number" v-model="slotsHolder" />
+      <input
+        class="slots-entry"
+        type="number"
+        v-model="slotsHolder"
+        :min="minSlots"
+        :max="maxSlots"
+      />
       <button v-on:click="handleSubmit">Submit</button>
     </div>
   </div>
@@ -27,18 +44,21 @@ export default {
       slots: 6,
       slotsHolder: 6,
       diameter: 800,
+      minSlots: 3,
+      maxSlots: 19,
     };
   },
   computed: {
     radius() {
-      return this.diameter / 2;
+      // + 10 just for extra pixels to manipulate margin with
+      return this.diameter / 2 + 10;
     },
     wedgePerimeter() {
       // Not really calculating the perimeter here. Since we are using triangles to create
       // the wedges, the perimeter is not applicable here. So we have to create a larger
       // triangle that will encompass the whole area of the wedge.
       const angle = 360 / this.slots / 2;
-      return Math.tan((angle * Math.PI) / 180) * this.radius;
+      return Math.ceil(Math.tan((angle * Math.PI) / 180) * this.radius);
     },
     wedgeAngle() {
       return 360 / this.slots;
@@ -46,7 +66,22 @@ export default {
   },
   methods: {
     handleSubmit() {
-      this.slots = parseInt(this.slotsHolder, 10);
+      const val = parseInt(this.slotsHolder, 10);
+      if (val >= this.maxSlots) {
+        this.slots = this.maxSlots;
+        this.slotsHolder = this.maxSlots;
+      } else if (val <= this.minSlots) {
+        this.slots = this.minSlots;
+        this.slotsHolder = this.minSlots;
+      } else {
+        this.slots = parseInt(this.slotsHolder, 10);
+      }
+    },
+    colorController(index) {
+      if (this.slots % 2 === 1 && index === this.slots) {
+        return '#9ba4b4';
+      }
+      return index % 2 === 0 ? '#14274e' : '#394867';
     },
   },
 };
@@ -64,7 +99,8 @@ export default {
   margin: 0 auto;
   height: var(--diameter);
   width: var(--diameter);
-  background-color: gray;
+  min-width: var(--diameter);
+  background-color: white;
   border-radius: 50%;
   overflow: hidden;
 }
@@ -79,8 +115,10 @@ export default {
   transform-origin: top left;
   height: 0;
   width: 0;
-  border-bottom: var(--wedge-perimeter) solid transparent;
-  border-right: var(--radius) solid orange;
+  margin-left: -1px;
+  border-bottom: var(--wedge-perimeter) solid;
+  border-right: var(--radius) solid;
+  border-color: inherit;
 }
 .wedge::after {
   content: '';
@@ -88,9 +126,21 @@ export default {
   transform-origin: bottom left;
   height: 0;
   width: 0;
-  margin-top: calc(-1 * var(--wedge-perimeter));
-  border-top: var(--wedge-perimeter) solid transparent;
-  border-right: var(--radius) solid green;
+  margin-left: -1px;
+  margin-top: var(--wedge-offset);
+  border-top: var(--wedge-perimeter) solid;
+  border-right: var(--radius) solid;
+  border-color: inherit;
+}
+.wedge-label {
+  position: absolute;
+  display: flex;
+  align-items: center;
+  z-index: 10;
+  font-size: 2em;
+  height: var(--label-height);
+  margin-top: var(--wedge-offset);
+  margin-left: 300px;
 }
 .slots-entry {
   margin-left: 8px;
