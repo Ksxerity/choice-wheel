d464688686dfe19a8351b9da104ddfb93877c897
Kyle Ki <itskyleki@gmail.com>
2025-01-12 11:33:35 -0500

    Migrated from vue 2 to vue 3. Is able to run but the migration broke most functionality

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,9 @@
-<template>
-  <div id="app">
-    <Wheel></Wheel>
-  </div>
-</template>
-
-<script>
-import Wheel from './components/wheel.vue';
-
-export default {
-  name: 'App',
-  components: {
-    Wheel,
-  },
-};
+<script setup lang="ts">
+  import Wheel from './components/wheel.vue';
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  padding-top: 60px;
-  padding-bottom: 20px;
-  overflow: hidden;
-  background-color: #fff2e0;
-}
-</style>
+<template>
+  <main>
+    <Wheel />
+  </main>
+</template>
